e398910af85983cb614d26fea8025611d6a97341
qiumozhou <2675607101@qq.com>
2021-01-31 00:52:25 -0800

    添加登录页

--- vue_admin/src/router/index.js
+++ vue_admin/src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Login from "@/components/login/login.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+
       path: '/',
-   
-    }
+
+    },
+        {
+      name:"login",
+      path: '/login',
+      component: Login
+    },
   ]
 })
